99ac941e8b8b955cea2fdcff22e4a10301a17d9a
satwikakota <satwika.kota@gmail.com>
2020-08-13 12:57:31 -0400

    Added Slingshot Images

--- sketch.js
+++ sketch.js
@@ -34,9 +34,9 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,60);
     platform= new Ground(150,305,300,170); 
-    sling= new Slingshot(bird.body,{x:100,y:100}); 
+    sling= new Slingshot(bird.body,{x:200,y:60}); 
 
 
 }
--- slingshot.js
+++ slingshot.js
@@ -4,18 +4,37 @@ class Slingshot{
             bodyA:body1,
             pointB:point2,
             stiffness:0.04,
-            length:10,
+            length:10
+            
         }
+        this.sling1=loadImage("sprites/sling1.png"); 
+        this.sling2=loadImage("sprites/sling2.png"); 
+        this.sling3=loadImage("sprites/sling3.png"); 
         this.slingshot= Constraint.create(options); 
         this.point2=point2
         World.add(world,this.slingshot); 
     }
     display(){
+        image(this.sling1,200,22); 
+        image(this.sling2,170,15); 
+        
         if(this.slingshot.bodyA!=null){
             push()
+            stroke("#301608"); 
             var posA= this.slingshot.bodyA.position;
             var posB= this.point2
-            line(posA.x,posA.y,posB.x, posB.y); 
+            if(posA.x<220){
+                strokeWeight(7); 
+                line(posA.x-25,posA.y-3,posB.x-25, posB.y-3); 
+                line(posA.x-25,posA.y,posB.x+30, posB.y-3); 
+                image(this.sling3,posA.x-30,posA.y-10,15,30);
+            }
+            else{
+                strokeWeight(3); 
+                line(posA.x+25,posA.y-3,posB.x-25, posB.y-3); 
+                line(posA.x+25,posA.y,posB.x+30, posB.y-3); 
+                image(this.sling3,posA.x+30,posA.y-10,15,30); 
+            }
             pop()
         }
     }
